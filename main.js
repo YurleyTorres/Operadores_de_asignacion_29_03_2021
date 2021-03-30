546615dd52340ca20dbbee431b24000468e8d4e5
YurleyTorres <marly.torres.silva@gmail.com>
2021-03-29 21:14:41 -0500

    29-03-2021

--- main.js
+++ main.js
@@ -1,5 +1,164 @@
 
 
+function operadorDeExponenciacion(x, y){
+    console.warn(`Parametros iniciales`);
+    console.log(`Parametro x : ${x}`);
+    console.log(`Parametro y : ${y}`);
+    x **= y; //Operador de exponenciacion Abreviado
+//  x = x ** y; //Operador de exponenciacion Significado
+    console.warn(`Operador exponenciacion`);
+    console.log(`Parametro x : ${x} RESULTADO`);
+    console.log(`Parametro y : ${y}`);
+}
+function operadorDeModulo(x, y){
+    console.warn(`Parametros iniciales`);
+    console.log(`Parametro x : ${x}`);
+    console.log(`Parametro y : ${y}`);
+    x %= y; //Operador de Modulo Abreviado
+//  x = x % y; //Operador de Modulo Significado
+    console.warn(`Operador Modulo`);
+    console.log(`Parametro x : ${x} RESULTADO`);
+    console.log(`Parametro y : ${y}`);
+}
+function operadorDeDivision(x, y){
+    console.warn(`Parametros iniciales`);
+    console.log(`Parametro x : ${x}`);
+    console.log(`Parametro y : ${y}`);
+    x /= y; //Operador de Division Abreviado
+//  x = x / y; //Operador de Division Significado
+    console.warn(`Operador Division`);
+    console.log(`Parametro x : ${x} RESULTADO`);
+    console.log(`Parametro y : ${y}`);
+}
+function operadorDeMultiplicacion(x, y){
+    console.warn(`Parametros iniciales`);
+    console.log(`Parametro x : ${x}`);
+    console.log(`Parametro y : ${y}`);
+    x *= y; //Operador de Multiplicacion Abreviado
+//  x = x * y; //Operador de Multiplicacion Significado
+    console.warn(`Operador Multiplicacion`);
+    console.log(`Parametro x : ${x} RESULTADO`);
+    console.log(`Parametro y : ${y}`);
+}
+function operadorDeSustracion(x, y){
+    console.warn(`Parametros iniciales`);
+    console.log(`Parametro x : ${x}`);
+    console.log(`Parametro y : ${y}`);
+    x -= y; //Operador de Sustracion Abreviado
+//  x = x - y; //Operador de Sustracion Significado
+    console.warn(`Operador Sustracion`);
+    console.log(`Parametro x : ${x} RESULTADO`);
+    console.log(`Parametro y : ${y}`);
+}
+function operadorDeAdicion(x, y){
+    console.warn(`Parametros iniciales`);
+    console.log(`Parametro x : ${x}`);
+    console.log(`Parametro y : ${y}`);
+    x += y; //Operador de Adicion Abreviado
+//  x = x + y; //Operador de Adicion Significado
+    console.warn(`Operador Adicion`);
+    console.log(`Parametro x : ${x} RESULTADO`);
+    console.log(`Parametro y : ${y}`);
+}
+function operadorDeAsignacion(x, y){
+    console.warn(`Parametros iniciales`);
+    console.log(`Parametro x : ${x}`);
+    console.log(`Parametro y : ${y}`);
+    x = y; //Operador de Asignacion Abreviado
+//  x = y; //Operador de Asignacion Significado
+    console.warn(`Operador Asignacion`);
+    console.log(`Parametro x : ${x} RESULTADO`);
+    console.log(`Parametro y : ${y}`);
+}
+let x = '5';
+let y = 2;
+operadorDeAsignacion(x,y);
+operadorDeAdicion(x,y);
+operadorDeSustracion(x,y);
+operadorDeMultiplicacion(x,y);
+operadorDeDivision(x,y);
+operadorDeModulo(x,y);
+operadorDeExponenciacion(x,y);
+
+
+
+// let x = '50';
+// let y = 3;
+
+
+// console.warn(x);
+// console.warn(y);
+//  x **= y; //Operador de exponenciacion Abreviado
+//x = x ** y; //Operador de exponenciacion Significado
+// console.log(x);
+// console.log(x);
+// console.log(y);
+
+
+// console.warn(x);
+// console.warn(y);
+// x %= y; //Operador de Modulo Abreviado
+// x = x % y; //Operador de Modulo Abreviado
+// console.log(x);
+// console.log(y);
+
+
+
+
+
+// console.warn(x);
+// console.warn(y);
+// x /= y; //Operador de division Abreviado
+// x = x / y; //Operador de division Significado
+// console.log(x);
+// console.log(y);
+
+
+
+
+
+
+// console.warn(x);
+// console.warn(y);
+// x *= y; //Operador de multiplicacion Abreviado
+// x = x * y; //Operador de multiplicacion Significado
+// console.log(x);
+// console.log(y);
+
+
+
+
+// console.warn(x);
+// console.warn(y);
+// x -= y; //Operador de sustracion Abreviado
+// x = x - y; //Operador de sustracion Significado
+// console.log(x);
+// console.log(y);
+
+
+
+
+
+// console.warn(x);
+// console.warn(y);
+// x += x; //Operador de adicion Abreviado
+// x = x + x; //Operador de adicion Significado
+// console.log(x);
+// console.log(y);
+
+
+
+
+// console.warn(x);
+// console.warn(y);
+// x = y; //Operador de aignacion
+// console.log(x);
+// console.log(y);
+
+
+
+
+
 // let x=1;
 // let y=2;
 // let z='unir';
@@ -20,10 +179,11 @@
 // console.log(x);
 // console.log(y);
 // console.log(result);
-let x = parseInt('f', 16);
-let y = 2;
-console.warn(x);
-console.warn(x);
-x *= y;
 
-console.log(x);
+
+
+
+
+
+
+
