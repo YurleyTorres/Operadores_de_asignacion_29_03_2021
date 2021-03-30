0eb93478db2b234e545fd546da6102637596ee8a
YurleyTorres <marly.torres.silva@gmail.com>
2021-03-29 20:43:35 -0500

    29-03-2021

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+// let x=1;
+// let y=2;
+// let z='unir';
+// let result;
+// console.warn(x);
+// console.warn(y);
+// console.warn(z);
+
+// x = y; //Operador de asignación
+// console.log(x);
+// console.log(y);
+// x += y; //Operador de asignación cuando uno de los datos concatena
+// result = x+y; // Para sumar
+
+// z-=y;//Operador de sustraccion abreviado
+// result=x-y;//Operador de sustraccion significado
+
+// console.log(x);
+// console.log(y);
+// console.log(result);
+let x = parseInt('f', 16);
+let y = 2;
+console.warn(x);
+console.warn(x);
+x *= y;
+
+console.log(x);
